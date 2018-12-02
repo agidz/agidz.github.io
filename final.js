a34f9d0fcdaa5744086217ad77d67168cda0d26d
agidz <andrew.gidzinski@gmail.com>
2018-12-02 07:01:42 -0500

    stage change

--- final.js
+++ final.js
@@ -10,6 +10,7 @@ var sketchProc = function (processingInstance) {
         var bugDebug = true;
         var eyeDebug = true;
 
+        
         var mPressed = false; //represents if the mouse was down or up
         var mClicked = false;
         //Player Stats Saved
